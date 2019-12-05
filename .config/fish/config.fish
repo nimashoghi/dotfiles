8eaa54a9c2e2e3efcaa9e4ef43e1303dd494ceba
Nima Shoghi <nimashoghi@gmail.com>
2019-12-05 14:41:36 -0500

    Remove fish_prompt.fish file Inline fish_prompt into config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,28 @@ function fish_user_key_bindings
     bind \e\[3\;5~ kill-word
 end
 
+function fish_prompt
+    set --local exit_code $status
+
+    echo ""
+
+    if test $exit_code -ne 0
+        set_color red
+    else
+        set_color $fish_color_cwd
+    end
+    echo (prompt_pwd)
+    set_color normal
+
+    if test $exit_code -ne 0
+        set_color red
+    else
+        set_color brblue
+    end
+    echo -n "λ "
+    set_color normal
+end
+
 set fish_greeting
 
 if test -d "/mnt/c/Users/Admin"
