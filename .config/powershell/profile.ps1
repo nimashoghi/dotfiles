45e52a6d57102433d63d56722f42289e25130adc
Nima Shoghi <nimashoghi@gmail.com>
2020-03-28 04:05:05 -0400

    Added PowerShell config

---
+++ .config/powershell/profile.ps1
@@ -0,0 +1,34 @@
+Set-PSReadLineKeyHandler -Key Enter -Function AcceptLine
+Set-PSReadLineKeyHandler -Chord Shift+Enter -Function AddLine
+
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key `0 -Function MenuComplete
+
+Set-PSReadLineKeyHandler -Chord Ctrl+LeftArrow -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function ForwardWord
+
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+LeftArrow -Function SelectBackwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+RightArrow -Function SelectForwardWord
+
+Set-PSReadLineKeyHandler -Key Home -Function BeginningOfline
+Set-PSReadLineKeyHandler -Key End -Function EndOfLine
+
+Set-PSReadLineKeyHandler -Chord Shift+Home -Function SelectBackwardsLine
+Set-PSReadLineKeyHandler -Chord Shift+End -Function SelectLine
+
+Set-PSReadLineKeyHandler -Key Backspace -Function BackwardDeleteChar
+Set-PSReadLineKeyHandler -Key Delete -Function DeleteChar
+
+# Set-PSReadLineKeyHandler -Chord Ctrl+Backspace -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Chord Ctrl+Delete -Function DeleteWord
+
+Set-PSReadlineKeyHandler -Key UpArrow -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+}
+Set-PSReadlineKeyHandler -Key DownArrow -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+}
+
+New-Alias open ii
