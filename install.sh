ee08d9de5e95c4e9086e94de4b681d225567cb1d
Nima Shoghi <nimashoghi@gmail.com>
2020-03-29 18:34:26 -0400

    Remove git folder in dotfiles repo clone

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ pwsh -c "Install-Module -Force -Name Set-PsEnv -Scope CurrentUser"
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+rm -rf $SCRIPTPATH/.git
 cp -rT $SCRIPTPATH ~
 rm -rf $SCRIPTPATH ~/install.sh
