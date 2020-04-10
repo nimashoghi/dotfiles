56bf3f6afe97e60ca677e4b9a1ae35f2fc4e05bf
Nima Shoghi <nimashoghi@gmail.com>
2020-04-10 19:06:59 -0400

    Added -AcceptLicense for PSUnitUtilCompleters install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pwsh -c "Install-Module -Force -Name posh-git -Scope CurrentUser"
 pwsh -c "Install-Module -Force -Name oh-my-posh -Scope CurrentUser"
-pwsh -c "Install-Module -Force -Name PSUnixUtilCompleters -Scope CurrentUser"
+pwsh -c "Install-Module -Force -Name PSUnixUtilCompleters -Scope CurrentUser -AcceptLicense"
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
