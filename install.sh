d3d268cc859bfdfdebf808046efaf0a3d0207677
Nima Shoghi <nimashoghi@gmail.com>
2020-05-29 15:23:55 -0400

    Get pre-release version of posh-git

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pwsh -c "Install-Module -Force -Name posh-git -Scope CurrentUser"
+pwsh -c "Install-Module -Force -Name posh-git -Scope CurrentUser -AllowPrerelease"
 pwsh -c "Install-Module -Force -Name oh-my-posh -Scope CurrentUser"
 pwsh -c "Install-Module -Force -Name Microsoft.PowerShell.UnixCompleters -Scope CurrentUser"
 
