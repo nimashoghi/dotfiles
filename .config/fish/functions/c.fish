48aa4a40309a9eb78c41222821971f11255fdd94
Nima Shoghi <nimashoghi@gmail.com>
2025-11-09 21:47:40 -0500

    remove no longer used fish functions

--- .config/fish/functions/c.fish
+++
@@ -1,3 +0,0 @@
-function c --description 'Launch VS Code'
-    _vscode_priority_finder $argv
-end
--- .config/fish/functions/code-insiders.fish
+++
@@ -1,3 +0,0 @@
-function code-insiders --description 'Launch VS Code'
-    _vscode_priority_finder $argv
-end
